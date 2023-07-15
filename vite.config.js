7ef8da60dfa1587fb3e90b5e7fc391bcebddff1e
张富营 <fuying.zhang@shuyun.com>
2023-07-15 23:59:51 +0800

    feat: 修改ci文件

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,23 @@ export default defineConfig({
   define: {
     "process.env": {},
   },
+  rollupOptions: {
+    // 自定义 Rollup 配置
+    plugins: [
+      // 处理 CSS 文件的插件
+      {
+        name: "rollup-plugin-css-only",
+        transform(code, id) {
+          if (id.endsWith(".css")) {
+            // 将 CSS 文件转换为 JS 模块导出
+            const transformedCode = `export default ${JSON.stringify(code)};`;
+            return {
+              code: transformedCode,
+              map: { mappings: "" },
+            };
+          }
+        },
+      },
+    ],
+  },
 });
