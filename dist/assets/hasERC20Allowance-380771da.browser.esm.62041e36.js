8cf24ef0c31c105a2f58f8c50e2cb7c4698b0e86
张富营 <fuying.zhang@shuyun.com>
2023-07-15 23:51:30 +0800

    feat: 修改ci文件

--- dist/assets/hasERC20Allowance-380771da.browser.esm.62041e36.js
+++
@@ -1,1 +0,0 @@
-import{y as c,aH as d}from"./index.123e7009.js";async function w(a,e,n){const r=a.getProvider(),t=new c(r,e,d,{}),s=await a.getSignerAddress(),o=a.readContract.address;return(await t.readContract.allowance(s,o)).gte(n)}export{w as h};
