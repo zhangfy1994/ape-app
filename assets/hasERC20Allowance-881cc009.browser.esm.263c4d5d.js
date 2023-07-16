99bc8f5d32cb071e5197d2b9688e4419f6cdd5d4
zhangfy1994 <83523814+zhangfy1994@users.noreply.github.com>
2023-07-16 04:29:34 +0000

    Deploying to gh-pages from @ zhangfy1994/ape-app@76bdd5bebfc15b8711fa8de0362ff86f40f44e90 🚀

---
+++ assets/hasERC20Allowance-881cc009.browser.esm.263c4d5d.js
@@ -0,0 +1,1 @@
+import{x as c,aG as d}from"./index.5b003a35.js";async function w(a,e,n){const r=a.getProvider(),t=new c(r,e,d,{}),s=await a.getSignerAddress(),o=a.readContract.address;return(await t.readContract.allowance(s,o)).gte(n)}export{w as h};
