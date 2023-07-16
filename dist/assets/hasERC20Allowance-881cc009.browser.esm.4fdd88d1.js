e0cbf16c107831c25e00eacf951e42eaa005ec58
张富营 <fuying.zhang@shuyun.com>
2023-07-16 12:11:58 +0800

    feat: 使用dist

---
+++ dist/assets/hasERC20Allowance-881cc009.browser.esm.4fdd88d1.js
@@ -0,0 +1,1 @@
+import{x as c,aG as d}from"./index.eec712e0.js";async function w(a,e,n){const r=a.getProvider(),t=new c(r,e,d,{}),s=await a.getSignerAddress(),o=a.readContract.address;return(await t.readContract.allowance(s,o)).gte(n)}export{w as h};
